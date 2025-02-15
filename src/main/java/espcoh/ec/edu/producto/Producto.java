bd84fca161c1fb35c9dc29a6219cc667b07c5740
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2025-02-15 12:52:24 -0500

    Correccion del Examen

---
+++ src/main/java/espcoh/ec/edu/producto/Producto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espcoh.ec.edu.producto;
+
+/**
+ *
+ * @author user
+ */
+public class Producto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
